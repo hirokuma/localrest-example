bb4feb4e0f89fd652091eb0473d94bd0362b41e3
hirokuma <hiro9ma@gmail.com>
2025-11-19 14:54:47 +0900

    return anyhow

--- rest2/src/server.rs
+++ rest2/src/server.rs
@@ -1,16 +1,19 @@
 use std::collections::HashMap;
 
+use anyhow::Result;
 use axum::{
-    Json, extract::{FromRequest, State, rejection::JsonRejection}, http::StatusCode, response::IntoResponse
+    Json,
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
 };
-use serde::Serialize;
 use serde_json::json;
 
-use crate::{cmd, CommandHandler, RestReq, RestRes};
+use crate::{CommandHandler, RestReq, RestRes, cmd};
 
 #[derive(Clone)]
 pub struct AppState {
-    handlers: HashMap<&'static str, CommandHandler>
+    handlers: HashMap<&'static str, CommandHandler>,
 }
 
 impl AppState {
@@ -24,14 +27,42 @@ impl AppState {
     pub async fn handler(
         State(state): State<Self>,
         Json(value): Json<RestReq>,
-    ) -> impl IntoResponse {
+    ) -> Result<Json<RestRes>, AppError> {
         let res = if let Some(func) = state.handlers.get(value.command.as_str()) {
-            func(&value).expect("JSON response")
+            func(&value)?
         } else {
-            // return Err(ApiError{status: StatusCode::INTERNAL_SERVER_ERROR, message: "what?".to_string()});
-            panic!("だめ")
+            return Err(AppError(anyhow::anyhow!("だめ")));
         };
-        Json(res)
+        Ok(Json(res))
+    }
+}
+
+// https://github.com/tokio-rs/axum/blob/b1ef45469bf8ffa334e86ddd12e7f4d4b82fa1ab/examples/anyhow-error-response/src/main.rs
+pub struct AppError(anyhow::Error);
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let payload = json!({
+            "message": "not success",
+            "from": self.0.to_string(),
+        });
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            axum::Json(payload),
+        )
+            .into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
     }
 }
 
